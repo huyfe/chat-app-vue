140269fae996c3e228de00b43b3fb2bcbcdb82af
unknown <gwyn@namtech.sg>
2022-06-09 22:41:16 +0700

    Fix UI homepage

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,8 +1,10 @@
 <template>
-  <div class="home flex-1 fade-in">
+  <div class="home flex-1 fade-in h-full">
     <div class="flex flex-col h-full items-center justify-center">
-      <h1 class="text-4xl font-second">Welcome To My Chat App !</h1>
-      <img :src="require('@/assets/images/chat.webp')" alt="" class="w-2/5" />
+      <h1 class="text-2xl font-second mb-5 text-blue-dark">
+        Welcome To Blue Chat !
+      </h1>
+      <img :src="require('@/assets/images/chat-2.webp')" alt="" class="w-2/5" />
     </div>
   </div>
 </template>
